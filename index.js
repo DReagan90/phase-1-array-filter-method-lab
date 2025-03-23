01bc09bdf0f5a2218774ab4138f4645d654b6b49
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-23 19:48:51 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+// index.js
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, prefix) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(prefix.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
